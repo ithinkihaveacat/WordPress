22a0094e46a5fc6086d2767a6cc79e4123bbda95
Michael Stillwell <mjs@beebo.org>
2019-10-09 22:00:26 +0100

    Reformat

--- beebo-theme/src/functions.php
+++ beebo-theme/src/functions.php
@@ -1,7 +1,6 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'beebo_theme_enqueue_styles' );
 function beebo_theme_enqueue_styles() {
- 
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array(),
