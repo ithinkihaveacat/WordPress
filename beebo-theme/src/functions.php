3b6002f96f776159830b7059cae98f9bd197388c
Michael Stillwell <mjs@beebo.org>
2020-04-01 21:47:11 +0100

    Attempt to slim down CSS

--- beebo-theme/src/functions.php
+++ beebo-theme/src/functions.php
@@ -1,11 +1,16 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'beebo_theme_enqueue_styles' );
-function beebo_theme_enqueue_styles() {
- 
+add_action( 'wp_enqueue_scripts', 'beebo_enqueue_scripts' );
+function beebo_enqueue_scripts() {
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array(),
         wp_get_theme()->get('Version')
     );
+    // wp_dequeue_style( 'wp-block-library' );
+    // wp_dequeue_style( 'wp-block-library-theme' );
+}
+add_action( 'wp_enqueue_scripts', 'beebo_enqueue_scripts_last', 100);
+function beebo_enqueue_scripts_last() {
+    wp_dequeue_style( 'twentynineteen-print-style' );
 }
 ?>
