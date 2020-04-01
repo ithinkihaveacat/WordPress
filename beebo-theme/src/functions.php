b967f1b16fd4cfc20f5515b6f447672d01659f9d
Michael Stillwell <mjs@beebo.org>
2020-04-01 23:14:42 +0100

    Remove more styles

--- beebo-theme/src/functions.php
+++ beebo-theme/src/functions.php
@@ -6,11 +6,12 @@ function beebo_enqueue_scripts() {
         array(),
         wp_get_theme()->get('Version')
     );
-    // wp_dequeue_style( 'wp-block-library' );
-    // wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
 }
 add_action( 'wp_enqueue_scripts', 'beebo_enqueue_scripts_last', 100);
 function beebo_enqueue_scripts_last() {
     wp_dequeue_style( 'twentynineteen-print-style' );
+    wp_dequeue_style( 'amp-default' );
 }
 ?>
